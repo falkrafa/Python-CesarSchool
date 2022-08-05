b5d2e3e62131f725721f50a3231c00a4eacced6d
falkrafa <rmf2@cesar.school>
2022-08-05 00:43:52 -0300

    adicionando atividade de anagrama

---
+++ aula/anagrama.py
@@ -0,0 +1,21 @@
+import os
+os.system("cls")
+
+print("Teste de anagrama.")
+print()
+
+nome1= input("Digite uma palavra: ")
+nome2= input("Digite outra palavra: ")
+
+lista=list(nome1)
+lista.sort()
+lista1=list(nome2)
+lista1.sort()
+
+if lista == lista1:
+    print("\nEssas palavras sao anagramas!\n")
+else:
+    print("\nEssas palavras não sao anagramas!\n")
+    
+
+    
